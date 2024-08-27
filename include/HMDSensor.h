cde7109da13a84080e28af3734426fc354bd567a
GeorgiadisK <georgiadeskonstandinos@gmail.com>
2024-08-27 18:59:21 +0300

    configure HMDSensor class

--- include/HMDSensor.h
+++ include/HMDSensor.h
@@ -1,18 +1,26 @@
 #pragma once
-#include "Config.h"
+#include "config.h"
 
 class HMDSensor : public Sensor
 {
-    private:
-        uint8_t dataPin;
-        uint8_t powerPin;
-    public:
-        HMDSensor(uint8_t powerPin, uint8_t dataPin);
-        ~HMDSensor() override;
-        float read() override;
-        bool isCloseToThreshold(float &data) override;
-        bool checkData(float &data) override;
-        void setupHMD();
-        uint8_t returnPin() override;
-        
+private:
+    static uint8_t counter;
+    static HMDSensor* instances[];
+
+    uint8_t dataPin;
+    uint8_t powerPin;
+    const char *name;
+    bool status;
+public:
+    HMDSensor(uint8_t powerPin, const char *name,uint8_t dataPin);
+    ~HMDSensor() override;
+    bool setupSensor();
+    float read() override;
+    bool checkData(float &data) override;
+    uint8_t getPin() override;
+    const char *getName() const override;
+    virtual bool getStatus() override;
+    static uint8_t getCounter();
+    static HMDSensor* getInstance(int index);
+
 };
--- src/HMDSensor.cpp
+++ src/HMDSensor.cpp
@@ -1,51 +1,72 @@
-#include "Config.h"
+#include "config.h"
 
-HMDSensor::HMDSensor(uint8_t powerPin, uint8_t dataPin) : powerPin{HMD_POWER_PIN_CONFIG}, dataPin{HMD_DATA_PIN_CONFIG}
-{
+uint8_t HMDSensor::counter{0};
+HMDSensor *HMDSensor::instances[SENSORS_NUM] = {nullptr};
+
+HMDSensor::HMDSensor(uint8_t powerPin, const char *name, uint8_t dataPin) : powerPin{powerPin}, name{name}, dataPin{dataPin} {
+    instances[counter++] = this;
 }
 
-HMDSensor::~HMDSensor()
-{
+HMDSensor::~HMDSensor() {
 }
 
-float HMDSensor::read()
-{
-    digitalWrite(HMD_POWER_PIN_CONFIG, HIGH);
-    delay(10);
-    float moisture = analogRead(HMD_DATA_PIN_CONFIG);
-    moisture = map(moisture, 22, 4000, 0, 100);
-    digitalWrite(HMD_POWER_PIN_CONFIG, LOW);
+bool HMDSensor::setupSensor() {
+    for (size_t i = 0; i < validADCpinCount; i++) {
+        if (dataPin == validADCpins[i]) {
+            pinMode(dataPin, INPUT);
+            pinMode(powerPin, OUTPUT);
+            float value = read();
+            while (!checkData(value)) {
+                Serial.print("HMD SETUP STATUS: MODULE: ");
+                Serial.print(getName());
+                Serial.println(" ERROR. Check the wiring/Sensor failure");
+                vTaskDelay(1000 / portTICK_PERIOD_MS);
+            }
+            if (value >= HMD_THRESHOLD)
+                pumps_on = true;
+            else
+                pumps_on = false;
+            return true;
+        }
+    }
+    Serial.printf("In HMDSensor setup() %hhu is not and ADC pin. Check data sheet or the validADCpins list.\n", dataPin);
+    return false;
+}
+
+float HMDSensor::read() {
+    digitalWrite(powerPin, HIGH);
+    float moisture = analogRead(dataPin);
+    moisture = (moisture / 4095.0) * 100.0;
+    digitalWrite(powerPin, LOW);
     return moisture;
 }
 
-void HMDSensor::setupHMD()
-{
-    // Serial.println("HMD Sensor installation...");
-    pinMode(HMD_POWER_PIN_CONFIG, OUTPUT);
-    pinMode(HMD_DATA_PIN_CONFIG, INPUT);
-    // Serial.println("HMD STATUS...OK");
-    // Serial.print("HMD SENSOR DATA PIN...");
-    // Serial.println(returnPin());
+bool HMDSensor::checkData(float &data) {
+    if (data > 100 || data < 0) {
+        status = false;
+        return status;
+    } else {
+        status = true;
+        return status;
+    }
 }
 
-bool HMDSensor::isCloseToThreshold(float &data)
-{
-    if (data >= HMD_THRESHOLD)
-        return true;
-    if(data <= (HMD_THRESHOLD - OFFSET))
-        return false;
-    return true;
-}
-
-bool HMDSensor::checkData(float &data)
-{
-    if(data > 100 || data < 0)
-        return false;
-    else
-        return true;
-}
-
-uint8_t HMDSensor::returnPin()
-{
+uint8_t HMDSensor::getPin() {
     return dataPin;
+}
+
+const char *HMDSensor::getName() const {
+    return name;
+}
+
+uint8_t HMDSensor::getCounter() {
+    return counter;
+}
+
+bool HMDSensor::getStatus() {
+    return status;
+}
+
+HMDSensor *HMDSensor::getInstance(int index) {
+    return (index > counter ? nullptr : instances[index]);
 }
