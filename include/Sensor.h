17ecf7a73e2d9343e69293e444de16c9b36bd099
GeorgiadisK <georgiadeskonstandinos@gmail.com>
2024-08-27 19:01:43 +0300

    configure Sensor class

--- include/Sensor.h
+++ include/Sensor.h
@@ -4,9 +4,12 @@
 class Sensor
 {
 public:
+    virtual bool setupSensor() = 0;
     virtual float read() = 0;
-    virtual bool isCloseToThreshold(float &data) = 0;
     virtual bool checkData(float &data) = 0;
-    virtual uint8_t returnPin() = 0;
+    virtual uint8_t getPin() = 0;
     virtual ~Sensor() = default;
+    virtual const char *getName() const = 0;
+    virtual bool getStatus() = 0;
+    // virtual void logError(const char* name) = 0;
 };
