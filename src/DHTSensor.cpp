771e355c81479963f68f807370676b020ea06430
GeorgiadisK <georgiadeskonstandinos@gmail.com>
2024-08-27 18:58:00 +0300

    add briefs and configure methods
    implement all methods from DHTSensor.h

--- src/DHTSensor.cpp
+++ src/DHTSensor.cpp
@@ -1,68 +1,68 @@
-#include "Config.h"
+#include "config.h"
 
-DHTSensor::DHTSensor(uint8_t pin, const uint8_t DHT_TYPE) : pin{pin}, DHT_TYPE{DHT_TYPE}, dht{pin, DHT_TYPE}
-{
+/**
+ * @class DHTSensor
+ * @brief Handles DHT sensor operations including initialization, reading, and validation.
+ */
+
+uint8_t DHTSensor::counter{0};
+DHTSensor *DHTSensor::instances[SENSORS_NUM] = {nullptr};
+
+DHTSensor::DHTSensor(uint8_t pin, const char *name, const uint8_t DHT_TYPE) : pin{pin}, name{name}, DHT_TYPE{DHT_TYPE}, dht{pin, DHT_TYPE} {
+    instances[counter++] = this;
 }
 
-DHTSensor::~DHTSensor()
-{
+DHTSensor::~DHTSensor() {
 }
 
-bool DHTSensor::isCloseToThreshold(float &data)
-{
-    if (data >= DHT_THRESHOLD)
-        return true;
-    if (data <= (DHT_THRESHOLD - OFFSET))
-        return false;
-    return true;
-}
-
-float DHTSensor::read()
-{
-    sensor_t sensor;
+float DHTSensor::read() {
     sensors_event_t event;
     dht.temperature().getEvent(&event);
     return event.temperature;
 }
 
-uint8_t DHTSensor::returnPin()
-{
+uint8_t DHTSensor::getPin() {
     return pin;
 }
 
-void DHTSensor::setupDHT()
-{
-    // Serial.println("DHT11 Sensor installation...");
-    pinMode(DHT_DATA_PIN_CONFIG, INPUT);
+bool DHTSensor::setupSensor() {
+    pinMode(pin, INPUT);
     dht.begin();
     dht.temperature().getSensor(&sensor);
-    while (isnan(read()))
-    {
-        digitalWrite(0, HIGH);
-        // Serial.println("DHT11 SETUP STATUS: ERROR. Check the wiring/Sensor failure");
-        delay(1000);
-    }
-    digitalWrite(0, LOW);
-    digitalWrite(1, HIGH);
-    // Serial.println("DHT11 STATUS...OK");
-    // Serial.print("DHT TYPE...");
-    // Serial.println(DHT_TYPE_CONFIG);
-    // Serial.print("DHT SENSOR DATA PIN...");
-    // Serial.println(returnPin());
-}
+    float temperature = read();
 
-bool DHTSensor::checkData(float &data)
-{
-    if (!isnan(data))
-    {
-        digitalWrite(0, LOW);
-        digitalWrite(1, HIGH);
-        return true;
-    }
-    else
-    {
-        digitalWrite(1, LOW);
-        digitalWrite(0, HIGH);
+    if (isnan(temperature)) {
+        Serial.printf("DHT11 SETUP STATUS: MODULE: %s ERROR. Check the wiring/Sensor failure\n", getName());
         return false;
     }
+
+    if (temperature >= DHT_THRESHOLD) {
+        fans_on = false;
+        servos_on = false;
+    } else {
+        fans_on = true;
+        servos_on = true;
+    }
+    return true;
+}
+
+bool DHTSensor::checkData(float &data) {
+    status = !isnan(data);  // if data are not nan
+    return status;
+}
+
+const char *DHTSensor::getName() const {
+    return name;
+}
+
+uint8_t DHTSensor::getCounter() {
+    return counter;
+}
+
+bool DHTSensor::getStatus() {
+    return status;
+}
+
+DHTSensor *DHTSensor::getInstance(int index) {
+    return (index >= counter ? nullptr : instances[index]);
 }
