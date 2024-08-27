ec84edfc875c8406638803ad7ebd1bece8e30e67
GeorgiadisK <georgiadeskonstandinos@gmail.com>
2024-08-27 18:57:03 +0300

    configure DHTSensor class
    remove 2 parameters constructor, remove isCloseToThreshold(), remove returnPin() and remove setupDHT().
    add object counter, array of instances and basic getters

--- include/DHTSensor.h
+++ include/DHTSensor.h
@@ -8,18 +8,27 @@
 class DHTSensor : public Sensor
 {
 private:
+    static uint8_t counter;
+    static DHTSensor* instances[];
+
     DHT_Unified dht;
     uint8_t DHT_TYPE;
     uint8_t pin;
     sensor_t sensor;
     sensors_event_t event;
+    const char *name;
+    bool status;
 
 public:
-    DHTSensor(uint8_t pin, uint8_t DHT_TYPE);
+    DHTSensor(uint8_t pin, const char *name, uint8_t DHT_TYPE);
     ~DHTSensor() override;
+    bool setupSensor() override;
     float read() override;
-    bool isCloseToThreshold(float &data) override;
     bool checkData(float &data) override;
-    uint8_t returnPin() override;
-    void setupDHT();
+    uint8_t getPin() override;
+    const char *getName() const override;
+    virtual bool getStatus() override;
+    // static 
+    static uint8_t getCounter();
+    static DHTSensor* getInstance(int index);
 };
